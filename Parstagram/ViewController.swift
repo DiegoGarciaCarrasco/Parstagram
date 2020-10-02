781d072a1130a046657336f03eeed226cf6cd68f
Diego Garcia <diegoalonzo_prod@hotmail.com>
2020-10-01 21:24:12 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Diego Garcia on 10/1/20.
+//  Copyright © 2020 garciadiegoTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
